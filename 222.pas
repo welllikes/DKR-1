451a6770f88503c4be7e97f018f5cc61deb6ccfb
welllikes <156796062+welllikes@users.noreply.github.com>
2024-03-21 14:12:24 +0300

    Add files via upload

---
+++ 222.pas
@@ -0,0 +1,29 @@
+﻿uses GraphABC, Fractal;
+
+var (m, wx, wy, g) := (100, 300, 300, 10);
+
+Procedure KeyDown(k: integer);
+begin
+case k of
+VK_Down: wy -= 10;
+VK_Up: wy += 10;
+VK_Left: wx += 10;
+VK_Right: wx -= 10;
+VK_Escape: halt(1); // выход
+VK_A: if m < 500 then m += 10;
+VK_Z: if m > 20 then m -= 10;
+VK_S: if g < 15 then g += 1;
+VK_X: if g > 2 then g -= 1;
+end;
+Window.Clear;
+Fractal.ris(wx - m, wy, wx + m, wy, g); // Использование процедуры из модуля
+Redraw;
+end;
+
+begin
+SetWindowCaption('Фракталы: Кривая Дракона');
+SetWindowSize(700, 512);
+LockDrawing;
+KeyDown(0);
+OnKeyDown += KeyDown;
+end.
---
+++ Fractal.pas
@@ -0,0 +1,24 @@
+﻿unit Fractal;
+
+interface
+
+uses GraphABC;
+
+procedure ris(x1, y1, x2, y2, k: integer); // параметризация
+
+implementation
+
+procedure ris(x1, y1, x2, y2, k: integer);
+begin
+if k > 0 then // база
+begin
+var xn := (x1 + x2) div 2 + (y2 - y1) div 2; // декомпозиция
+var yn := (y1 + y2) div 2 - (x2 - x1) div 2;
+ris(x1, y1, xn, yn, k - 1);
+ris(x2, y2, xn, yn, k - 1);
+end
+else
+line(x1, y1, x2, y2);
+end;
+
+end.
